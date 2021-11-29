325fc45cadf29a4d7a813e33260e50eeed4b46cf
Joshua Cotton <joshcotton2001@gmail.com>
2021-11-28 21:04:42 -0500

    added GUI

---
+++ GUI.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+from tkinter import *
+root = tk.Tk()
+Frm = Frame(root)
+canvas = tk.Canvas(root, height=500, width=500, bg="#263D42")
+canvas.pack()
+Label(Frm, text='Search:').pack(side=LEFT)
+modify = Entry(Frm)
+modify.pack(side=LEFT, fill=BOTH, expand=1)
+modify.focus_set()
+button = Button(Frm, text='Enter')
+button.pack(side=RIGHT)
+Frm.pack(side=TOP)
+
+
+def find():
+    print(modify.get())
+
+
+button.config(command=find)
+root.mainloop()
