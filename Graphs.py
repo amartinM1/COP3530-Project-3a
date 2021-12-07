3461b59d9f06579d8f6187210ffdcaf0836d6a63
alexi <alexiamartin0812@gmail.com>
2021-12-07 15:43:49 -0500

    pushing graphy.py edits

--- Graphs.py
+++ Graphs.py
@@ -11,6 +11,7 @@ class Adjlist:
     parser.read_file()
     obj = Game.Game()
 
+    # constructor
     def __init__(self, _dict={}, _count=0, fro='', to=''):
         self.dict = _dict
         self.count = _count
@@ -25,15 +26,20 @@ class Adjlist:
                 print(key, "is already a vertex in the graph")
             else:
                 self.count += 1
-                self.dict[key] = []
+                self.dict[key] = list()
 
     def edge(self):
 
-       for key in self.dict.keys():
-            for values in parser.unorderedMap.values():
-                for i in range(len(values.categories)):
-                     if parser.unorderedMap[key].categories == values.categories:
-                # access each individual attribute of the game
-                #weight = 
-                 # node = [values, weight]
-              # var = self.dict[key].append[node]
+        for key in self.dict.keys():
+            weight = 0
+            temp = parser.unorderedMap[key]
+            for tag in temp.steamspy_tags:
+                for x in temp.tags_map[tag]:
+
+                    if x not in self.dict[key]:
+                        temp_tuple = (x, weight)
+                        self.dict[key].append[temp_tuple]
+                    if x in self.dict[key]:
+                        weight = weight + 1
+                        temp_tuple = (x, weight)
+                        self.dict[key].append[temp_tuple]
