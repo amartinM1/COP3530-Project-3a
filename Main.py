84505bed69df6930d1c6cc97564cc848754fa57f
EPalmer220 <54413756+EPalmer220@users.noreply.github.com>
2021-12-07 19:12:30 -0500

    Improved main

--- Main.py
+++ Main.py
@@ -3,5 +3,25 @@ import Game
 from Game import Game
 from CSVReader import CSVReader
 
-reader1 = CSVReader("steam.csv")
-reader1.read_file()
+def game_info(title):
+    if title in reader.unorderedMap:
+        game = reader.unorderedMap[title]
+        game.print()
+
+        tags = game.get_tags()
+        print(f'\nTags:')
+        for tag in tags:
+            print(f'{tag}: {reader.tags_map[tag]}')
+
+
+
+
+
+
+
+reader = CSVReader("steam.csv")
+reader.read_file()
+
+print("Hello! Please search a game:")
+title = input()
+game_info(title)
